72aa231da45dc700d3c42361e23233595fd30159
Matin Amani <mtin.amani@gmail.com>
2023-10-17 14:54:24 +0330

    build: install persian dict for cspell

--- src/app/pages/HelloWorld.tsx
+++ src/app/pages/HelloWorld.tsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom'
 import { routes } from '../router/routes'
 
+import { t } from '../i18n'
+
 const HelloWorld = () => {
 	return (
 		<>
-			<h1>HelloWorld</h1>
+			<h1>{t('messages.HELLO_WORLD')}</h1>
 			<Link to={routes.USERS.path}>Go to Users</Link>
 		</>
 	)
