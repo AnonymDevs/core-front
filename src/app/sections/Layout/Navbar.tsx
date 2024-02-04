2e35466a0f829fdad464dafc6ffa501ec029abfb
Matin Amani <mtin.amani@gmail.com>
2024-02-04 14:05:00 +0330

    feat: add theme change button to navbar

--- src/app/sections/Layout/Navbar.tsx
+++ src/app/sections/Layout/Navbar.tsx
@@ -6,11 +6,19 @@ import {
 	NavigationMenuList,
 	navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu'
+import { Button } from '@/components/ui/button'
+import { Moon, Sun } from 'lucide-react'
+
 import { routes } from '@/enums'
+import { useTheme } from '@/providers/theme'
 
 const Navbar = () => {
+	const { setTheme, theme } = useTheme()
+
+	const handleThemeChange = () => setTheme(theme === 'dark' ? 'light' : 'dark')
+
 	return (
-		<div className='sticky top-0 z-50 w-full px-16 py-2 border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
+		<div className='sticky top-0 z-50 w-full px-16 py-2 border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 flex'>
 			<NavigationMenu>
 				<NavigationMenuList>
 					<NavigationMenuItem>
@@ -29,6 +37,11 @@ const Navbar = () => {
 					</NavigationMenuItem>
 				</NavigationMenuList>
 			</NavigationMenu>
+			<div className='flex flex-1 items-center justify-between space-x-2 md:justify-end'>
+				<Button variant='ghost' onClick={handleThemeChange}>
+					{theme === 'dark' ? <Sun /> : <Moon />}
+				</Button>
+			</div>
 		</div>
 	)
 }
