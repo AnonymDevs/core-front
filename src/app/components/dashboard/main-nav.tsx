e3fd7bd801e6e1cfc3fb8d74fc32bcf29cd1b123
Matin Amani <mtin.amani@gmail.com>
2024-02-07 03:14:51 +0330

    fix: remove unnecessary interface

--- src/app/components/dashboard/main-nav.tsx
+++ src/app/components/dashboard/main-nav.tsx
@@ -10,24 +10,9 @@ export function MainNav({
 			className={cn('flex items-center space-x-4 lg:space-x-6', className)}
 			{...props}>
 			<Link
-				to='/examples/dashboard'
+				to='/'
 				className='text-sm font-medium transition-colors hover:text-primary'>
 				Overview
-			</Link>
-			<Link
-				to='/examples/dashboard'
-				className='text-sm font-medium text-muted-foreground transition-colors hover:text-primary'>
-				Customers
-			</Link>
-			<Link
-				to='/examples/dashboard'
-				className='text-sm font-medium text-muted-foreground transition-colors hover:text-primary'>
-				Products
-			</Link>
-			<Link
-				to='/examples/dashboard'
-				className='text-sm font-medium text-muted-foreground transition-colors hover:text-primary'>
-				Settings
 			</Link>
 		</nav>
 	)
